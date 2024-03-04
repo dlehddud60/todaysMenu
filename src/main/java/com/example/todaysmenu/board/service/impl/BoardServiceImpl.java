9d775075aa1fb76e641f1f17ac676395ddfd32c1
dlehddud60 <dlehddud60@naver.com>
2024-03-04 10:47:05 +0900

    파일업로드 미첨부시 null처리하기

--- src/main/java/com/example/todaysmenu/board/service/impl/BoardServiceImpl.java
+++ src/main/java/com/example/todaysmenu/board/service/impl/BoardServiceImpl.java
@@ -64,28 +64,27 @@ public class BoardServiceImpl implements BoardService {
     }
     @Override
     public void insert(BoardDTO freeboarddto, CommonFileDTO commonFileDTO, HttpServletRequest request)throws FileExtensionExaption, FileSizeExaption {
-
-
-        int resut = boardRepository.insert(freeboarddto);
-
-
-        commonFileMethod(freeboarddto, commonFileDTO,commonFileRepository, request);
+        boolean fileResult = commonFileDTO.getTcft_multi_file().get(0).isEmpty();
+        boardRepository.insert(freeboarddto);
+        if(!fileResult) {
+            commonFileMethod(freeboarddto, commonFileDTO,commonFileRepository, request);
+        }
     }
 
-
+    @Override
+    public void update(BoardDTO boardDTO, CommonFileDTO commonFileDTO, HttpServletRequest request) throws FileExtensionExaption, FileSizeExaption {
+        boolean fileResult = commonFileDTO.getTcft_multi_file().get(0).isEmpty();
+        boardRepository.update(boardDTO);
+        if(!fileResult) {
+            commonFileMethod(boardDTO, commonFileDTO,commonFileRepository, request);
+        }
+    }
 
     @Override
     public void update(BoardDTO boardDTO) {
         boardRepository.update(boardDTO);
     }
 
-    @Override
-    public void update(BoardDTO boardDTO, CommonFileDTO commonFileDTO, HttpServletRequest request) throws FileExtensionExaption, FileSizeExaption {
-        log.info("=================update호출====================");
-        boardRepository.update(boardDTO);
-        commonFileMethod(boardDTO, commonFileDTO,commonFileRepository, request);
-
-    }
 
     @Override
     public void delete(int tfb_seq) {
